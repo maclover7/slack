c5cb0eff8ed58bf829eff3fcef97524b4b7035e8
Jon Moss <me@jonathanmoss.me>
2016-06-04 22:49:58 -0400

    Add displayWindow()

--- app.js
+++ app.js
@@ -2,20 +2,33 @@ var App = function(electron) {
   this.electron = electron
   // Module to create native browser window.
   this.BrowserWindow = electron.BrowserWindow
+
+  this.windows = {}
+  let activeWindow
 }
 
 App.prototype.boot = function() {
   // Create the browser window.
   mainWindow = new this.BrowserWindow({ width: 800, height: 600 })
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-  mainWindow.webContents.openDevTools()
+  mainWindow.hide()
+  this.windows['main'] = mainWindow
+  this.displayWindow('main')
+}
+
+App.prototype.displayWindow = function(name) {
+  newWindow = this.windows[name]
+  activeWindow = newWindow
+
+  newWindow.show()
+  newWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  activeWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    activeWindow = null
   })
 }
 

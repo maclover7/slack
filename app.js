cdb6eb0e1fb9e80bf824132917d1c8df1518ed8a
Jon Moss <me@jonathanmoss.me>
2016-06-05 10:49:16 -0400

    Improve team windows code

--- app.js
+++ app.js
@@ -7,32 +7,38 @@ var App = function(electron) {
   this.Menu = electron.Menu
 
   this.configPath = electron.app.getPath('userData') + '/maclover7-slack.json'
-  this.teams = {}
+  this.teams = []
   this.windows = {}
 
-  let activeWindow
+  this.activeWindow = null
 }
 
 App.prototype.boot = function() {
   // Load in teams configuration
   this._loadConfiguration()
   this._setupHomePage()
+  this._setupWindows()
   this._setupMenu()
+  this.displayWindow('home')
 }
 
 App.prototype.displayWindow = function(name) {
   newWindow = this.windows[name]
-  activeWindow = newWindow
+
+  if (this.activeWindow) {
+    this.activeWindow.hide()
+  }
 
   newWindow.show()
-  newWindow.webContents.openDevTools()
+  this.activeWindow = newWindow
 
+  that = this
   // Emitted when the window is closed.
-  activeWindow.on('closed', function () {
+  this.activeWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    activeWindow = null
+    that.activeWindow = null
   })
 }
 
@@ -55,25 +61,64 @@ App.prototype._loadConfiguration = function() {
       console.log("Created configuration file and wrote base configuration.")
     })
 
-    this.teams = {}
+    this.teams = []
   }
 }
 
 App.prototype._setupHomePage = function() {
-  // Create the browser window.
+  // Create the browser window
   homeWindow = new this.BrowserWindow({ width: 800, height: 600 })
   homeWindow.loadURL(`file://${__dirname}/index.html`)
   homeWindow.hide()
   this.windows['home'] = homeWindow
-  this.displayWindow('home')
+  homeWindow.hide()
 }
 
 App.prototype._setupMenu = function() {
   var appMenu = new AppMenu(this)
   var appMenu = appMenu.build()
 
+  appMenu[1].submenu.push(
+    { type: 'separator' }
+  )
+
+  this._loadConfiguration()
+  that = this
+
+  this.teams.forEach(function(team) {
+    appMenu[1].submenu.push({
+      label: team,
+      click() {
+        that.displayWindow(team)
+      }
+    })
+  })
+
   var menu = this.Menu.buildFromTemplate(appMenu)
   this.Menu.setApplicationMenu(menu)
+}
+
+App.prototype._setupTeamWindow = function(team) {
+  // Create the browser window.
+  teamWindow = new this.BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+
+  teamWindow.loadURL(`https://${team}.slack.com`)
+  teamWindow.hide()
+  this.windows[team] = teamWindow
+}
+
+App.prototype._setupWindows = function() {
+  that = this
+
+  this.teams.forEach(function(team) {
+    that._setupTeamWindow(team)
+  })
 }
 
 module.exports = App

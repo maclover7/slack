653ae15ab524b0243c1bcf102860a6fcde8c21d8
Jon Moss <me@jonathanmoss.me>
2016-06-04 23:08:04 -0400

    Add configuration file, and updated homepage

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ var App = function(electron) {
   this.electron = electron
   // Module to create native browser window.
   this.BrowserWindow = electron.BrowserWindow
+  this.configPath = electron.app.getPath('userData') + '/maclover7-slack.json'
 
+  this.teams = {}
   this.windows = {}
   let activeWindow
 }
 
 App.prototype.boot = function() {
+  // Load in teams configuration
+  this._loadConfiguration()
+
   // Create the browser window.
-  mainWindow = new this.BrowserWindow({ width: 800, height: 600 })
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
-  mainWindow.hide()
-  this.windows['main'] = mainWindow
-  this.displayWindow('main')
+  homeWindow = new this.BrowserWindow({ width: 800, height: 600 })
+  homeWindow.loadURL(`file://${__dirname}/index.html`)
+  homeWindow.hide()
+  this.windows['home'] = homeWindow
+  this.displayWindow('home')
 }
 
 App.prototype.displayWindow = function(name) {
@@ -30,6 +35,29 @@ App.prototype.displayWindow = function(name) {
     // when you should delete the corresponding element.
     activeWindow = null
   })
+}
+
+App.prototype._loadConfiguration = function() {
+  baseConfig = {
+    teams: {}
+  }
+
+  var fs = require('fs')
+
+  if (fs.existsSync(this.configPath)) {
+    var contents = fs.readFileSync(this.configPath, 'utf8')
+    this.teams = JSON.parse(contents).teams
+  } else {
+    fs.writeFile(this.configPath, JSON.stringify(baseConfig), function(err) {
+      if (err) {
+        return console.log(err)
+      }
+
+      console.log("Created configuration file and wrote base configuration.")
+    })
+
+    this.teams = {}
+  }
 }
 
 module.exports = App
